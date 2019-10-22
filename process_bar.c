f0d4b18ae334b6717f5c17c6d2e4d38c439052bd
Adam-Xi <321042835@qq.com>
2019-10-22 11:10:33 +0800

    进度条模拟实现小程序

---
+++ process_bar.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+
+int main()
+{
+    int i = 0;
+    char bar[102];
+    memset(bar, 0, sizeof(bar));
+    const char * lable = "|/-\\";
+    while(i <= 100)
+    {
+        printf("[%-100s][%d%%][%c]\r", bar, i, lable[i % 4]);
+        fflush(stdout);
+        bar[i++] = '#';
+        usleep(50000);
+    }
+    printf("\n");
+    printf("waiting");
+    for(i = 0; i < 3; i++)
+    {
+        printf(".\r");
+        usleep(1000000);
+    }
+    printf("\nloading engine...\n");
+
+    
+    return 0;
+}
