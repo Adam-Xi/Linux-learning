b7ca537b81b0cd31ca18ee1397780686b44301ea
Adam-Xi <321041835@qq.com>
2019-12-12 23:02:46 +0800

    测试线程池

---
+++ ThreadPool_test.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unistd.h>
+#include "threadPool.hpp"
+
+//这个类由用户自定制，需要依赖哪些数据都可以随意添加和修改
+class MyTask : public Task
+{
+public:
+    MyTask(int id)
+        :id_(id)
+    {}
+    void Run()
+    {
+        //执行用户自定制的逻辑
+        printf("id = %d\n", id_);
+    }
+private:
+    int id_;
+};
+
+int main()
+{
+    ThreadPool pool(10);
+    for(int i = 0; i < 20; i++)
+    {
+        pool.AddTask(new MyTask(i));
+    }
+    while(1)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
+
